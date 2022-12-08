93601748f806752aa8e82cc3167eaa79b98aeb42
KKucht <krzysztof.k2001.01@wp.pl>
2022-12-08 20:33:40 +0100

    changed data

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
 
 
 
 def main():
     pass
-
-
-
-
-
-
-
 
 
 
