ffc10e9078c9987613e0c5ee946544cf7e0469f9
KKucht <krzysztof.k2001.01@wp.pl>
2022-12-08 21:23:39 +0100

    display some map

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-
+def render():
+    pass
 
 def main():
     name_of_file = "data/2021_CO_1g.csv"
     df = pd.read_csv(name_of_file)
     data_top = df.head()
-    print(data_top)
+
+    im = plt.imread("data/mapa.jpg")
+    implot = plt.imshow(im)
+    plt.show()
 
 
 
